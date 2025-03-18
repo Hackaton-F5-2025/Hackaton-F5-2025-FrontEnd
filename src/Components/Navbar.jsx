a5b48e20d4ee31c1f126cc261a1c95bdaad711c4
Guadalupe Hani Schajris <chikaruwynn@MacBook-Pro-de-Guadalupe.local>
2025-03-18 22:59:22 +0100

    NavBar icons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaHome, FaShoppingCart } from "react-icons/fa";
+
 const Navbar = () => {
-    return (
-      <nav>
-        <h1>EcoPart</h1>
-      </nav>
-    );
-  };
-  
-  export default Navbar;
-  
+  return (
+    <nav className="navbar">
+      <Link to="/" className="nav-button">
+        <FaHome className="icon" />
+      </Link>
+      {/* Barra de búsqueda (crear)
+      <SearchBar /> */}
+
+      <button className="nav-button">Alquiler</button>
+      <Link to="/repuestos" className="nav-button">Repuestos</Link>
+      <button className="nav-button">Residuos Creativos</button>
+      <button className="nav-button">Inicio de sesión</button>
+
+      <button className="nav-button">
+        <FaShoppingCart className="icon" />
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
