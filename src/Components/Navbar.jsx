387f7d29b85dc6171795a311c185c73f0bb1b216
Guadalupe Hani Schajris <chikaruwynn@MacBook-Pro-de-Guadalupe.local>
2025-03-20 13:51:55 +0100

    fix: Colours Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import styles from './navbar.module.css';
-import { FaHome, FaShoppingCart } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import styles from "./navbar.module.css";
+import { FaHome, FaShoppingCart } from "react-icons/fa";
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.leftSection}>
-        <FaHome className={styles.icon} />
-        <input type="text" placeholder="Hinted search text" className={styles.searchBar} />
+        <Link to="/" className={styles.iconLink}>
+          <FaHome className={styles.icon} />
+        </Link>
+        <input type="text" placeholder="Buscar..." className={styles.searchBar} />
       </div>
       <div className={styles.links}>
         <span>Compra</span>
         <span>Venta</span>
-        <span>Componentes</span>
-        <span>Reparación</span>
+        <Link to="/repuestos" className={styles.link}>Repuestos</Link>
+        <span>Residuos Creativos</span>
       </div>
-      <div className={styles.rightSection}>
-        <button className={styles.loginButton}>Inicio sesión</button>
+      <div className={styles.loginButton}>
+        <span>Inicio de sesión</span>
         <FaShoppingCart className={styles.icon} />
       </div>
     </nav>
