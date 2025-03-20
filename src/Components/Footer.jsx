7840f0b96eccf18184aba7a24ed39bebb9634f6a
Guadalupe Hani Schajris <chikaruwynn@MacBook-Pro-de-Guadalupe.local>
2025-03-20 11:24:05 +0100

    Footer feature done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,24 @@
+import React from "react";
+import { FaInstagram, FaYoutube, FaLinkedin, FaXTwitter } from "react-icons/fa6";
+
 const Footer = () => {
-    return (
-      <footer>
-        <p>© 2025 EcoPart. Todos los derechos reservados.</p>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer className="footer-container">
+      <div className="footer-links">
+        <a href="#">Use Cases</a>
+        <a href="#">Explore</a>
+      </div>
+      <div className="footer-copyright">
+        &copy; {new Date().getFullYear()} EcoPart. All rights reserved.
+      </div>
+      <div className="footer-social">
+        <a href="#"><FaXTwitter /></a>
+        <a href="#"><FaInstagram /></a>
+        <a href="#"><FaYoutube /></a>
+        <a href="#"><FaLinkedin /></a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
